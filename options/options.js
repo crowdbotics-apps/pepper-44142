02cd0076dc9070c8a23e71925410e2db4a8dbdf2
Crowdbotics <team@crowdbotics.com>
2023-10-25 15:03:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pepper-44142",
+  url: "https://pepper-44142.botics.co",
+  api: "https://pepper-44142.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
